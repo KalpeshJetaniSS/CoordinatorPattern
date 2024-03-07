647c6ecf98433daeca3445f17ed4a708657a638b
kalpesh <kalpesh.jetani@smartsensesolutions.com>
2024-03-07 10:16:43 +0530

    Initial Commit

---
+++ CoordinatorPattern/CoordinatorPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorPatternApp.swift
+//  CoordinatorPattern
+//
+//  Created by Kalpesh on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
